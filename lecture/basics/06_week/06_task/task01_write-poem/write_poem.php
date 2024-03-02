248b3ecadee8e4ac9b7415f516b5c2e8a980f040
Adel Ansari <adelansari.a@gmail.com>
2024-03-02 17:54:57 +0200

    poem done

--- lecture/basics/06_week/06_task/task01_write-poem/write_poem.php
+++ lecture/basics/06_week/06_task/task01_write-poem/write_poem.php
@@ -1,5 +1,11 @@
 <?php
     // if the form has been sent, add the new verse to the file
+    if (isset($_POST['add-to-poem'])) {
+        $newVerse = $_POST['new-verse'];
+        $file = fopen('poem.txt', 'a') or die("Failed to open the file");;
+        fwrite($file, $newVerse . "\n");
+        fclose($file);
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +20,12 @@
     <div id="poem">
         <?php
             // read the poem from file and display here
+            $file = fopen('poem.txt', 'r');
+            while (!feof($file)) {
+                $verse = fgets($file);
+                echo $verse . "<br>";
+            }
+            fclose($file);
         ?>
     </div>
 
