96bd19ee7e109069d10aee8e97ee781de366980c
Adel Ansari <adelansari.a@gmail.com>
2024-03-05 12:25:44 +0200

    add editbook file

---
+++ lecture/basics/final_project/booksite/editbook.php
@@ -0,0 +1,6 @@
+<?php
+    // If the user is not logged in, redirect them back to login.php.
+
+    // Check the POST parameter "bookid". If it's set, edit the corresponding book from the data file.
+
+    // Redirect back to admin.php.
