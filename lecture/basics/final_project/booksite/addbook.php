002abed59b504775f1032b6ad49e49d0946280ca
Adel Ansari <adelansari.a@gmail.com>
2024-03-06 01:08:32 +0200

    filter book data with GET bookid and determine button text

--- lecture/basics/final_project/booksite/addbook.php
+++ lecture/basics/final_project/booksite/addbook.php
@@ -11,6 +11,18 @@ $books = json_decode(file_get_contents('books.json'), true);
 
 isset($_GET['bookid']) ? $bookid = $_GET['bookid'] : $bookid = count($books) + 1;
 
+// fetch book details for editing
+$bookDetails = [];
+foreach ($books as $book) {
+    if ($book['id'] == $bookid) {
+        $bookDetails = $book;
+        break;
+    }
+}
+
+// determine the button text
+$buttonText = isset($_GET['editbook']) ? 'Edit Book' : 'Add Book';
+
 // if the form has been sent, add the book to the data file
 // In order to protect against cross-site scripting attacks (i.e. basic PHP security), remove HTML tags from all input.
 // There's a function for that. E.g.
