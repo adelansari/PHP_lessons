fef3794fc279abc9e492c57b68131be088f13869
Adel Ansari <adelansari.a@gmail.com>
2024-03-05 17:00:00 +0200

    deleting book based on id

--- lecture/basics/final_project/booksite/deletebook.php
+++ lecture/basics/final_project/booksite/deletebook.php
@@ -1,7 +1,29 @@
 <?php
-    // If the user is not logged in, redirect them back to login.php.
+session_start();
 
-    // Check the POST parameter "bookid". If it's set, delete the corresponding book from the data file.
-    // Hint: array_diff will not work here, since you'd need to create the whole book "object". Find the index and use array_splice instead.
+// If the user is not logged in, redirect them back to login.php.
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header('Location: login.php');
+    exit;
+}
 
-    // Redirect back to admin.php.
+// Check the POST parameter "bookid". If it's set, delete the corresponding book from the data file.
+// Hint: array_diff will not work here, since you'd need to create the whole book "object". Find the index and use array_splice instead.
+if (isset($_POST['bookid'])) {
+    $bookid = $_POST['bookid'];
+
+    $books = json_decode(file_get_contents('books.json'), true);
+
+    foreach ($books as $index => $book) {
+        if ($book['id'] == $bookid) {
+            array_splice($books, $index, 1);
+            break;
+        }
+    }
+
+    file_put_contents('books.json', json_encode($books));
+}
+
+// Redirect back to admin.php.
+header('Location: admin.php');
+exit;
